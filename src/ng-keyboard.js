76fbdeee6c89a2f195f19af1c5dfba8d569cb3c0
Bob Fanger <bfanger@gmail.com>
2014-09-15 18:55:19 +0200

    Implemented horizontal keyboard navigation

--- src/ng-keyboard.js
+++ src/ng-keyboard.js
@@ -221,21 +221,50 @@ angular.module('keyboard').directive('kbList', function (KbListController) {
                 hasFocus = false;
                 $scope.$apply();
             });
+            /**
+             * Check if an ClientRect is in given direction.
+             * Allows for keyboard navigation based on an elements relative visual location.
+             *
+             * @param {string} direction 'up', 'left', 'right' or 'down',
+             * @param {ClientRect} currentRect The position of the current item.
+             */
+            function checkLocation(direction, currentRect, targetRect) {
+                if (direction === 'left' && targetRect.left < currentRect.left) {
+                    return true;
+                }
+                if (direction === 'up' && targetRect.top < currentRect.top) {
+                    return true;
+                }
+                if (direction === 'right' && targetRect.left > currentRect.left) {
+                    return true;
+                }
+                if (direction === 'down' && targetRect.top > currentRect.top) {
+                    return true;
+                }
+                return false;
+            }
+            // { e.which: direction }
+            var directions = {
+                37: 'left',
+                38: 'up',
+                39: 'right',
+                40: 'down'
+            };
             el.on('keydown', function (e) {
                 if (angular.isUndefined(kbList.active)) {
                     return; // all keyboard action require an active listview-item
                 }
-
                 var changed = false;
                 if (e.which >= 37 && e.which <= 40) { // An arrow-key?
-                    // @todo Detect orientation
-                    var prevKey = kbList.orientation === 'horizontal' ? 37 : 38; // Left : Up
-                    var nextKey = kbList.orientation === 'horizontal' ? 39 : 40; // Right : Down
-
-                    if (e.which === prevKey) {
-                        changed = kbList.previous();
-                    } else if (e.which === nextKey) {
-                        changed = kbList.next();
+                    var location = kbList._locate(kbList.active);
+                    var currentRect = location.element[0].getBoundingClientRect();
+                    if (location.previous && checkLocation(directions[e.which], currentRect, location.previous.element[0].getBoundingClientRect())) {
+                        kbList.active = location.previous.model;
+                        changed = true;
+                    }
+                    if (location.next && checkLocation(directions[e.which], currentRect, location.next.element[0].getBoundingClientRect())) {
+                        kbList.active = location.next.model;
+                        changed = true;
                     }
                 } else if (kbList.mode !== 'list' && (e.which === 32 || e.which === 13)) { // Space || Enter
                     kbList.toggle(kbList.active);
